9272aff9bcdb33f664fe8d5a69838c8818b03488
PueyoLuciano <pueyo.luciano@gmail.com>
2015-09-18 17:13:51 -0300

    Create Letras.py

---
+++ Letras.py
@@ -0,0 +1,4 @@
+class Letras:
+	vocales = set(["a","e","i","o","u"])
+	consonantes = set(["b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","y","z"])
+	letras = vocales.union(consonantes)
