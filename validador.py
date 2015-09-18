9853754370dcb01dba0fec35ac842a9e75086766
PueyoLuciano <pueyo.luciano@gmail.com>
2015-09-18 17:16:36 -0300

    Update validador.py

--- validador.py
+++ validador.py
@@ -1,57 +1,69 @@
 def sincomparar(valoringresado,valoracomprar):
 	return True
 
+	
 class validador:
 	"""
 		Clase para ingresar datos por teclado de forma consistente.
-		metodos:
+		Metodos:
 		
-		-ingresar(tipo,funcionComparadora,*valoresAComparar)
-		-permite ingresar(valga la redundancia) valores y realizarle una comparacion logica.
-		-- tipo: es el type del dato a ingresar.
-		
-		-- funcionComparadora: (por defecto esta en modo sinComparar).
-		   este campo es un modo( detallado abajo) que especifica el tipo de comparasaion de los datos.
-		   
-		-- *valoresAComparar: valores usados para las evaluaciones logicas, dependiendo de la funcion comparadora
+		-> ingresar(tipo,funcionComparadora,*valoresAComparar)
+		-Permite ingresar(valga la redundancia) valores y realizarle una comparacion logica.
+		|
+		|-- tipo: es el type del dato a ingresar.
+		|
+		|-- funcionComparadora: (por defecto esta en modo sinComparar).
+		|   este campo es un modo( detallado abajo) que especifica el tipo de comparasaion de los datos.
+		|   
+		|-- *valoresAComparar: valores usados para las evaluaciones logicas, dependiendo de la funcion comparadora
 		    estos valores pueden variar.
 			
-		-ingresarBoolean(tipo,funcioncomparadora=sincomparar,*valoresacomparar)
-		-- Este metodo responde True o False a la pregunta: el valor ingresado, del tipo especificado, cumple con las validaciones especificadas?
-		-- tipo: es el type del dato a ingresar.
-		
-		-- funcionComparadora: (por defecto esta en modo sinComparar).
-		   este campo es un modo( detallado abajo) que especifica el tipo de comparasaion de los datos.
-		   
-		-- *valoresAComparar: valores usados para las evaluaciones logicas, dependiendo de la funcion comparadora
+		-> ingresarBoolean(tipo,funcioncomparadora=sincomparar,*valoresacomparar)
+		-Este metodo responde True o False a la pregunta: el valor ingresado, del tipo especificado, cumple con las validaciones especificadas?
+		|-- tipo: es el type del dato a ingresar.
+		|
+		|-- funcionComparadora: (por defecto esta en modo sinComparar).
+		|   este campo es un modo( detallado abajo) que especifica el tipo de comparasaion de los datos.
+		|   
+		|-- *valoresAComparar: valores usados para las evaluaciones logicas, dependiendo de la funcion comparadora
 		    estos valores pueden variar.
 		
-		-ingresarSINO()
-		-Este  metodo permite ingresar respuestas del tipo si o no.
+		-> ingresarVariable(variable)
+		-Pensado para ingresar datos usando la clase Variable.
+		|-- variable: este parametro tiene que ser del tipo Variable, ya que se van a usar atributos inherentes a este.
+		|
+		|-- es similar al ingresar, pero realiza la siguiente validacion:
+		 	Si posee valoresPosibles, valida que lo ingresado este en esa lista.
+		    Si no posee valoresPosibles, compara con validador.entre los valores maximo y minimos de la variable.
 		
-		-seleccionar(valores)
+		-> ingresarSINO()
+		-Este metodo permite ingresar respuestas del tipo si o no.
+		
+		-> seleccionar(valores)
 		- este metodo permite seleccionar un item de entre una lista de items, por su numero de orden.
 		
-		modos:
+		------------------------------------------------------------------------------------------------------------------------------------------------------
+		------------------------------------------------------------------------------------------------------------------------------------------------------
+		Modos:
 		-los modos son los que se indican en el campo funcionComparadora.
 		
-		-menor: valoringresado < valoresacomprar[0]
+		-> menor: valoringresado < valoresacomprar[0]
 		
-		-menorigual: valoringresado <= valoresacomprar[0]
+		-> menorigual: valoringresado <= valoresacomprar[0]
 		
-		-mayor: valoringresado > valoresacomprar[0]
+		-> mayor: valoringresado > valoresacomprar[0]
 		
-		-mayorigual: valoringresado >= valoresacomprar[0]
+		-> mayorigual: valoringresado >= valoresacomprar[0]
 		
-		-entre: valoresacomparar[0] <= valoringresado <= valoresacomprar[1]
+		-> entre: valoresacomparar[0] <= valoringresado <= valoresacomprar[1]
 		
-		-noentre: valoresacomparar[0] > valoringresado > valoresacomprar[1]
+		-> noentre: valoresacomparar[0] > valoringresado > valoresacomprar[1]
 		
-		-igual: valoresacomparar[0] = valoringresado
+		-> igual: valoresacomparar[0] = valoringresado
 				si valoresacomprar[0] es una lista:
 				valoringresado in valoresacomparar[0]
 				
-		-distinto: valoresacomparar[0] != valoringresado
+		-> distinto: valoresacomparar[0] != valoringresado
 				si valoresacomprar[0] es una lista:
 				valoringresado not in valoresacomparar[0]
 	
@@ -84,6 +96,29 @@ class validador:
 			else:
 				error = not(funcioncomparadora(valoringresado,valoresacomparar))
 			
+		return valoringresado
+	
+	@staticmethod
+	def ingresarVariable(variable):
+		error = True
+		while (error):
+			error = False
+			valoringresado = raw_input("> ")
+			
+			try:
+				tipo = type(variable.valor)
+				valoringresado = tipo(valoringresado)
+				
+			except (ValueError):
+				error = True
+			
+			else:
+				if(variable.valoresPosibles == []):
+					error = not(validador.entre(valoringresado,[variable.minimo,variable.maximo]))
+					
+				else:
+					error = not(validador.igual(valoringresado,variable.valoresPosibles))
+				
 		return valoringresado
 	
 	@staticmethod
@@ -145,7 +180,7 @@ class validador:
 	@staticmethod
 	def mayorigual(valoringresado,valoracomprar):
 		return True if valoracomprar[0] == None else valoringresado >= valoracomprar[0]
-	
+			
 	@staticmethod
 	def entre(valoringresado,valoresacomparar):
 		min = True if valoresacomparar[0] == None else valoringresado >= valoresacomparar[0]
