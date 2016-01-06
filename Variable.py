4af1a9ac2e67699462052b417eb77fcfd5db8268
PueyoLuciano <pueyo.luciano@gmail.com>
2016-01-06 13:58:38 -0300

    Update Variable.py

--- Variable.py
+++ Variable.py
@@ -1,31 +1,32 @@
 class Variable:
+
 	"""
-		Clase para generar variables con varios parametros, los datos de entrada valor y modificador son obligatorios.
-		el dato **parametros es un diccionario que contiene todos los parametros que son relevantes para la Variable.
-		
-		parametros:
-		-valor: contiene el valor concreto. puede ser cualquier cosa.
-		
-		-modificador: es la funcion que modifica a Variable. Esta pensada para ser usada con la clase Aplicacion, que tiene
-			un metodo modificar generico, el cual llama a este parametro.
-		
-		**Para cargar estos valores, hay que invocar a Variables con un parametro que se llame igual que alguno/s de esto/s: **
-		
-		-minimo: (opcional) puede ser que no tenga sentido, por ejemplo para un complex().
-		
-		-maximo: (opcional) idem minimo.
-		
-		-valoresPosibles: (opcional) Lista de valores posibles, util para conjuntos finitos.
-		
-		-flags: (opcional) diccionario con los flags que se quieran implementar.
-		
-		ejemplos de invocaciones:
-			Variable(15,funcionN1)
-			Variable(15.0,funcionN2,minimo=0)
-			Variable(8,funcionN3,minimo=0,maximo=17)
-			Variable("jpg",funcionN4,valoresPosibles=["jpg","bmp"],flags={"iterable":False})
-		
+    Clase para generar variables con varios parametros, los datos de entrada valor y modificador son obligatorios.
+    el dato **parametros es un diccionario que contiene todos los parametros que son relevantes para la Variable.
+    
+    parametros:
+    -valor: contiene el valor concreto. puede ser cualquier cosa.
+    
+    -modificador: es la funcion que modifica a Variable. Esta pensada para ser usada con la clase Aplicacion, que tiene
+        un metodo modificar generico, el cual llama a este parametro.
+    
+    **Para cargar estos valores, hay que invocar a Variables con un parametro que se llame igual que alguno/s de esto/s: **
+    
+    -minimo: (opcional) puede ser que no tenga sentido, por ejemplo para un complex().
+    
+    -maximo: (opcional) idem minimo.
+    
+    -valoresPosibles: (opcional) Lista de valores posibles, util para conjuntos finitos.
+    
+    -flags: (opcional) diccionario con los flags que se quieran implementar.
+    
+    ejemplos de invocaciones:
+        Variable(15,funcionN1)
+        Variable(15.0,funcionN2,minimo=0)
+        Variable(8,funcionN3,minimo=0,maximo=17)
+        Variable("jpg",funcionN4,valoresPosibles=["jpg","bmp"],flags={"iterable":False})
 	"""
+    
 	def __init__(self,valor, modificador, **parametros):
 		self.valor = valor
 		self.modificador = modificador
