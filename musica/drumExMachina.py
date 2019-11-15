37e65ebfac3f4194ef9d67c55378588a5d737bf3
Luciano Pueyo <pueyo.luciano@gmail.com>
2019-11-14 23:04:27 -0300

    asdasdasd

--- musica/drumExMachina.py
+++ musica/drumExMachina.py
@@ -3,6 +3,7 @@ import itertools
 import pygame.midi
 
 from ritmosEuclideos import *
+from midi_instruments import *
 
 """
     TODOS:
@@ -16,232 +17,6 @@ from ritmosEuclideos import *
         4 - Mejorar el armado de las partituras, listas de Secciones ?
 """
 
-midi_instruments = {
-    "Piano": [
-        "1.Acoustic Piano",
-        "2.BrtAcou Piano",
-        "3.ElecGrand Piano",
-        "4.Honky Tonk Piano",
-        "5.Elec.Piano 1",
-        "6.Elec.Piano 2",
-        "7.Harsichord",
-        "8.Clavichord",
-    ],
-
-    "Chromatic Percussion": [
-        "9.Celesta",
-        "10.Glockenspiel",
-        "11.Music Box",
-        "12.Vibraphone",
-        "13.Marimba",
-        "14.Xylophone",
-        "15.Tubular Bells",
-        "16.Dulcimer"
-    ],
-
-    "Organ": [
-        "17.Drawbar Organ",
-        "18.Perc. Organ",
-        "19.Rock Organ",
-        "20.Church Organ",
-        "21.Reed Organ",
-        "22.Accordian",
-        "23.Harmonica",
-        "24.Tango Accordian",
-    ],
-
-    "Guitar": [
-        "25.Acoustic Guitar",
-        "26.SteelAcous. Guitar",
-        "27.El.Jazz Guitar",
-        "28.Electric Guitar",
-        "29.El. Muted Guitar",
-        "30.Overdriven Guitar",
-        "31.Distortion Guitar",
-        "32.Guitar Harmonic",
-    ],
-    "Bass": [
-        "33.Acoustic Bass",
-        "34.El.Bass Finger",
-        "35.El.Bass Pick",
-        "36.Fretless Bass",
-        "37.Slap Bass 1",
-        "38.Slap Bass 2",
-        "39.Synth Bass 1",
-        "40.Synth Bass 2",
-    ],
-
-    "Strings": [
-        "41.Violin",
-        "42. Viola",
-        "43.Cello",
-        "44.Contra Bass",
-        "45.Tremelo Strings",
-        "46.Pizz. Strings",
-        "47.Orch. Strings",
-        "48.Timpani",
-    ],
-    
-    "Ensemble": [
-        "49.String Ens.1",
-        "50.String Ens.2",
-        "51.Synth.Strings 1",
-        "52.Synth.Strings 2",
-        "53.Choir Aahs",
-        "54. Voice Oohs",
-        "55. Synth Voice",
-        "56.Orchestra Hit",
-    ],
-    
-    "Brass": [
-        "57.Trumpet",
-        "58.Trombone",
-        "59.Tuba",
-        "60.Muted Trumpet",
-        "61.French Horn",
-        "62.Brass Section",
-        "63.Synth Brass 1",
-        "64.Synth Brass 2",
-    ],
-    
-    "Reed": [
-        "65.Soprano Sax",
-        "66.Alto Sax",
-        "67.Tenor Sax",
-        "68.Baritone Sax",
-        "69. Oboe",
-        "70.English Horn",
-        "71.Bassoon",
-        "72.Clarinet",
-    ],
-    
-    "Pipe": [
-        "73.Piccolo",
-        "74.Flute",
-        "75.Recorder",
-        "76.Pan Flute",
-        "77.Blown Bottle",
-        "78.Shakuhachi",
-        "79.Whistle",
-        "80.Ocarina",
-    ],
-
-    "Synth Lead": [
-        "81.Lead1 Square",
-        "82.Lead2 Sawtooth",
-        "83.Lead3 Calliope",
-        "84.Lead4 Chiff",
-        "85.Lead5 Charang",
-        "86.Lead6 Voice",
-        "87.Lead7 Fifths",
-        "88.Lead8 Bass Ld",
-    ],
-    
-    "Synth Pad": [
-        "89.Pad1 New Age",
-        "90.Pad2 Warm",
-        "91.Pad3 Polysynth",
-        "92.Pad4 Choir",
-        "93.Pad5 Bowed",
-        "94.Pad6 Metallic",
-        "95.Pad7 Halo",
-        "96.Pad8 Sweep",
-    ],
-    
-    "Synth F/X": [
-        "97.FX1 Rain",
-        "98.FX2 Soundtrack",
-        "99.FX3 Crystal",
-        "100.FX4 Atmosphere",
-        "101.FX5 Brightness",
-        "102.FX6 Goblins",
-        "103.FX7 Echoes",
-        "104.FX8 Sci-Fi",
-    ],
-
-    "Ethnic": [
-        "105.Sitar",
-        "106.Banjo",
-        "107.Shamisen",
-        "108.Koto",
-        "109.Kalimba",
-        "110. Bagpipe",
-        "111. Fiddle",
-        "112. Shanai",
-    ],
-    
-    "Percussive": [
-        "113.TinkerBell",
-        "114.Agogo",
-        "115.SteelDrums",
-        "116.Woodblock",
-        "117.TaikoDrum",
-        "118.Melodic Tom",
-        "119.SynthDrum",
-        "120.Reverse Cymbal",
-    ],
-    
-    "Sound F/X": [
-        "121.Guitar Fret Noise",
-        "122. Breath Noise",
-        "123.Seashore",
-        "124.BirdTweet",
-        "125.Telephone",
-        "126.Helicopter",
-        "127.Applause",
-        "128.Gunshot",
-    ],
-    
-    "Drum kit": [
-        '35 Acoustic Bass Drum',
-        '36 Bass Drum 1',
-        '37 Side Stick',
-        '38 Acoustic Snare',
-        '39 Hand Clap',
-        '40 Electric Snare',
-        '41 Low Floor Tom',
-        '42 Closed Hi-Hat',
-        '43 High Floor Tom',
-        '44 Pedal Hi-Hat',
-        '45 Low Tom',
-        '46 Open Hi-Hat',
-        '47 Low-Mid Tom',
-        '48 Hi-Mid Tom',
-        '49 Crash Cymbal 1',
-        '50 High Tom',
-        '51 Ride Cymbal 1',
-        '52 Chinese Cymbal',
-        '53 Ride Bell',
-        '54 Tambourine',
-        '55 Splash Cymbal',
-        '56 Cowbell',
-        '57 Crash Cymbal 2',
-        '58 Vibraslap',
-        '59 Ride Cymbal 2',
-        '60 Hi Bongo',
-        '61 Low Bongo',
-        '62 Mute Hi Conga',
-        '63 Open Hi Conga',
-        '64 Low Conga',
-        '65 High Timbale',
-        '66 Low Timbale',
-        '67 High Agogo',
-        '68 Low Agogo',
-        '69 Cabasa',
-        '70 Maracas',
-        '71 Short Whistle',
-        '72 Long Whistle',
-        '73 Short Guiro',
-        '74 Long Guiro',
-        '75 Claves',
-        '76 Hi Wood Block',
-        '77 Low Wood Block',
-        '78 Mute Cuica',
-        '79 Open Cuica',
-        '80 Mute Triangle',
-        '81 Open Triangle'
-    ]
-}
 
 class Nota:
     _lista = {
@@ -250,6 +25,17 @@ class Nota:
     }
 
     def __init__(self, nota_string):
+        """
+            nota_string: string que identifica una nota musical.
+                Debe respetar el siguiente orden:
+                
+                NOTA (en notacion americana) + ALTERACION(opcional / # o b) + OCTAVA (0 - 9) 
+                
+                Por ejemplo:
+                    C4 
+                    Bb1 (o su equivalente A#1)
+                    A#2 (o su equivalente Bb2)
+        """
         if type(nota_string) is int:
             self.notacion = 'sostenidos'
             self.indice = 127 if nota_string > 127 else (0 if nota_string < 0 else nota_string)
@@ -290,10 +76,10 @@ class Nota:
         return self
         
     def __repr__(self):
-        return self.__str__()
+        return self._lista[self.notacion][self.altura] + str(self.octava)
     
     def __str__(self):
-        return self._lista[self.notacion][self.altura] + str(self.octava)
+        return self.__repr__()
     
     @classmethod
     def octava(cls, nota):
